0007106158272c40e73f3e18e19c5bb862cabcbe
Dave Cross <dave@perlhacks.com>
2024-07-28 18:40:07 +0100

    Version 0.0.2

--- lib/App/LastStats.pm
+++ lib/App/LastStats.pm
@@ -11,7 +11,7 @@ class App::LastStats {
   use Getopt::Long;
   use JSON;
 
-  our $VERSION = '0.0.1';
+  our $VERSION = '0.0.2';
 
   field $username :param = 'davorg';
   field $period   :param = '7day';
