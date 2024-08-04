51238689b43f127c7b73003dd8018ea7b8b25667
Dave Cross <dave@perlhacks.com>
2024-08-04 14:02:35 +0100

    Prep release 0.0.5

--- lib/App/LastStats.pm
+++ lib/App/LastStats.pm
@@ -12,7 +12,7 @@ class App::LastStats {
   use Getopt::Long;
   use JSON;
 
-  our $VERSION = '0.0.4';
+  our $VERSION = '0.0.5';
 
   field $username   :param = 'davorg';
   field $period     :param = '7day';
